6ebcb750de3688a110137fed341df194be7676cf
Jibux <jibux@jibux.info>
2025-09-08 21:33:34 +0200

    Add backup_nas_pictures

--- scripts/backup/backup_nas.sh
+++ scripts/backup/backup_nas.sh
@@ -4,5 +4,6 @@
 ROOT_PATH="$(dirname "$(realpath "$0")")"
 
 "$ROOT_PATH/backup.sh" -vFf "$ROOT_PATH/backup_nas_data/"
+"$ROOT_PATH/backup.sh" -vFfc "$ROOT_PATH/backup_nas_pictures/"
 "$ROOT_PATH/backup.sh" -vFfc "$ROOT_PATH/backup_nas_media/"
 
