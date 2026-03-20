79f1dd6c2dc6996460e7083df34edcb0e6e25e73
Jibux <jibux@jibux.info>
2026-03-20 12:16:32 +0100

    Fixes for:
    * new ansible version
    * mint
    * misc

--- install_ansible.sh
+++ install_ansible.sh
@@ -68,7 +68,7 @@ uv pip install -r "$SCRIPT_ROOT_PATH/requirements.txt"
 (export bin_path=$HOME/.local/bin && curl -sfL https://direnv.net/install.sh | bash)
 direnv allow "$SCRIPT_ROOT_PATH"
 
-export PATH=$PATH:$HOME/.local/bin
+. "$VENV_DIR/bin/activate"
 
 ansible-galaxy install -r requirements.yml --force
 
